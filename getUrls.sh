1d1119b3525e7feccf348b70b1112762cb2c6837
mdaoustUdeS <54960692+mdaoustUdeS@users.noreply.github.com>
2022-08-25 00:37:58 -0400

    Add debuging info

--- getUrls.sh
+++ getUrls.sh
@@ -2,6 +2,12 @@
 
 outputDirectory=ical
 
+if [[ "$HORARIUS_KEY" ]]; then
+    echo "variable exist"
+else
+    echo 'no variable'
+fi
+
 url="https://www.gegi.usherbrooke.ca/horarius/icalendar?key=$HORARIUS_KEY"
 curl $url -o $outputDirectory/cal.ics
 
