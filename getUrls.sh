8a8531a8e0ed82e7a8a4868ba51fc76de8214a93
mdaoustUdeS <4569756-mdaoustUdeS@users.noreply.gitlab.com>
2022-08-26 02:32:12 -0400

    Generate csv fron ics

--- getUrls.sh
+++ getUrls.sh
@@ -6,9 +6,20 @@ set -o errtrace     # Make sure any error trap is inherited
 set -o nounset      # Disallow expansion of unset variables
 set -o pipefail     # Use last non-zero exit code in a pipeline
 
+# Setup folder
 outputDirectory="data"
 mkdir -p $outputDirectory
 
+#Download icalendar file
 url="https://www.gegi.usherbrooke.ca/horarius/icalendar?key=$HORARIUS_KEY"
 curl $url -o $outputDirectory/cal.ics
+
+#Ensure DTSTAMP is stable
 sed -i "s/DTSTAMP.*/DTSTAMP:20220825T000000Z/" $outputDirectory/cal.ics
+
+#Generate csv from ics
+dos2unix $outputDirectory/cal.ics
+awk -f ics2csv.awk $outputDirectory/cal.ics > $outputDirectory/cal.csv
+sort -o $outputDirectory/cal.csv{,}
+
+echo "$0 done!"
